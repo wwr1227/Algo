ed8deb609c3d5ccb90ba500af17925f55d80c477
wwr1227 <50442046@qq.com>
2013-09-16 09:58:28 +0800

    Create Threaded_binary_tree.java

---
+++ Tree/Threaded_binary_tree.java
@@ -0,0 +1,58 @@
+// 给一颗普通二叉树，设计算法得到最深最右叶子节点，优化算法达到空间复杂度O(1)。
+public TreeNode findMostDeepRightNode(TreeNode root){
+	if(root==null)
+		return root;
+	TreeNode cur=root;
+	TreeNode ans;
+	int curDeep=0;
+	int maxDeep=0;
+	while(cur!=null){
+		if(cur.left!=null){
+			TreeNode rightNode = cur.left;
+			int deep = 1;
+			while(rightNode.right!=null&&rightNode.right!=parent){
+				rightNode=rightNode.right;
+				deep++;
+			}
+			if(rightNode.right==null){
+				rightNode.right=parent;
+				cur = cur.left;
+				curDeep++;
+			}else if(rightNode.right==parent)
+				rightNode.right==null;
+				cur=cur.right;
+				curDeep-=deep;		
+		}else{			
+			if (cur_depth > max_depth) {
+				max_depth = cur_depth;
+				ans = root;
+			}
+			cur_depth++;
+			cur =cur.right;		
+		}
+	}
+	return ans;
+}
+//////////////////////////////////////////////////////////////////
+public TreeNode findMostDeepRightNode(TreeNode root){
+	if(root==null)
+		return root;
+	Queue<TreeNode> queue = new Queue<TreeNode>();
+	queue.offer(root);
+	queue.offer(null);
+	TreeNode ans=null, cur=null;
+	while(!queue.isEmpty()){
+		ans = cur;
+		cur = queue.poll();
+		if(cur==null){
+			if(queue.isEmpty()) break;
+			queue.offer(null);
+		}else{
+			if(cur.left!=null)
+				queue.offer(cur.left);
+			if(cur.right!=null)
+				queue.offer(cur.right);
+		}		
+	}
+	return ans;
+}
